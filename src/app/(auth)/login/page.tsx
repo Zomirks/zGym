884cb25eba35fa83b09397b874f8483cd559a614
Cyril Fischer <github@cyril-fischer.fr>
2026-02-22 16:48:23 +0100

    style: add shadcnui to login & register

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,7 +2,12 @@
 import { useState } from "react";
 import { authClient } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
+
+// Components
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Card } from "@/components/ui/card";
 
 const LoginPage = () => {
 	const router = useRouter();
@@ -13,7 +18,6 @@ const LoginPage = () => {
 
 	const handleSubmit = async (e: React.SyntheticEvent<HTMLFormElement>) => {
 		e.preventDefault();
-
 		setIsLoading(true);
 		setError(null);
 
@@ -26,48 +30,51 @@ const LoginPage = () => {
 		if(error) {
 			setError("Email ou mot de passe incorrect");
 			setIsLoading(false);
-			return;
+		} else {
+			router.push("/dashboard");
+			router.refresh();
 		}
-
-		router.push("/dashboard");
-		router.refresh();
 	};
 
 	return (
-		<div>
-			<h1>Connexion</h1>
+		<div className="flex-1 flex items-center justify-center">
+			<div className="w-full max-w-md flex flex-col gap-y-6">
+			<h1 className="text-2xl font-bold text-center">Connexion</h1>
 
-			<form onSubmit={handleSubmit}>
-				{error && (
-					<p>{error}</p>
-				)}
-				<div className="flex flex-col">
-					<label htmlFor="email">Adresse mail</label>
-					<input
-						id="email"
-						type="email"
-						value={email}
-						placeholder="Votre adresse mail"
-						onChange={(e) => setEmail(e.target.value)}
-					/>
-				</div>
+			<Card className="px-4 max-w-xl">
+				<form onSubmit={handleSubmit} className="flex flex-col gap-y-4">
+					{error && (
+						<p>{error}</p>
+					)}
+					<div className="flex flex-col gap-y-2">
+						<Label htmlFor="email">Adresse mail</Label>
+						<Input
+							id="email"
+							type="email"
+							value={email}
+							placeholder="Votre adresse mail"
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+					</div>
 
-				<div className="flex flex-col">
-					<label htmlFor="password">Mot de passe</label>
-					<input
-						id="password"
-						type="password"
-						value={password}
-						placeholder="Votre mot de passe"
-						onChange={(e) => setPassword(e.target.value)}
-					/>
-				</div>
+					<div className="flex flex-col gap-y-2">
+						<Label htmlFor="password">Mot de passe</Label>
+						<Input
+							id="password"
+							type="password"
+							value={password}
+							placeholder="Votre mot de passe"
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+					</div>
 
-				<Button
-					type="submit"
-					disabled={isLoading}
-				>{isLoading ? "Connexion en cours..." : "Se connecter"}</Button>
-			</form>
+					<Button
+						type="submit"
+						disabled={isLoading}
+					>{isLoading ? "Connexion en cours..." : "Se connecter"}</Button>
+				</form>
+			</Card>
+			</div>
 		</div>
 	)
 }
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 import { authClient } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Card } from "@/components/ui/card";
 
 const RegisterPage = () => {
 	const router = useRouter();
@@ -36,50 +39,54 @@ const RegisterPage = () => {
 	};
 
 	return (
-		<div>
-			<h1>Créer un compte</h1>
+		<div className="flex-1 flex items-center justify-center">
+			<div className="w-full max-w-md flex flex-col gap-y-6">
+				<h1 className="text-2xl font-bold text-center">Créer un compte</h1>
 
-			<form onSubmit={handleSubmit}>
-				{error && (
-					<p className="text-red-500">{error}</p>
-				)}
-				<div className="flex flex-col">
-					<label htmlFor="name">Nom</label>
-					<input
-						id="name"
-						type="name"
-						value={name}
-						placeholder="Votre nom"
-						onChange={(e) => setName(e.target.value)}
-					/>
-				</div>
-				<div className="flex flex-col">
-					<label htmlFor="email">Adresse mail</label>
-					<input
-						id="email"
-						type="email"
-						value={email}
-						placeholder="Votre adresse mail"
-						onChange={(e) => setEmail(e.target.value)}
-					/>
-				</div>
+				<Card className="px-4">
+					<form onSubmit={handleSubmit} className="flex flex-col gap-y-4">
+						{error && (
+							<p className="text-red-500">{error}</p>
+						)}
+						<div className="flex flex-col gap-y-2">
+							<Label htmlFor="name">Nom</Label>
+							<Input
+								id="name"
+								type="text"
+								value={name}
+								placeholder="Votre nom"
+								onChange={(e) => setName(e.target.value)}
+							/>
+						</div>
+						<div className="flex flex-col gap-y-2">
+							<Label htmlFor="email">Adresse mail</Label>
+							<Input
+								id="email"
+								type="email"
+								value={email}
+								placeholder="Votre adresse mail"
+								onChange={(e) => setEmail(e.target.value)}
+							/>
+						</div>
 
-				<div className="flex flex-col">
-					<label htmlFor="password">Mot de passe</label>
-					<input
-						id="password"
-						type="password"
-						value={password}
-						placeholder="Votre mot de passe"
-						onChange={(e) => setPassword(e.target.value)}
-					/>
-				</div>
+						<div className="flex flex-col gap-y-2">
+							<Label htmlFor="password">Mot de passe</Label>
+							<Input
+								id="password"
+								type="password"
+								value={password}
+								placeholder="Votre mot de passe"
+								onChange={(e) => setPassword(e.target.value)}
+							/>
+						</div>
 
-				<Button
-					type="submit"
-					disabled={isLoading}
-				>{isLoading ? "Création en cours..." : "S'enregistrer"}</Button>
-			</form>
+						<Button
+							type="submit"
+							disabled={isLoading}
+						>{isLoading ? "Création en cours..." : "S'enregistrer"}</Button>
+					</form>
+				</Card>
+			</div>
 		</div>
 	)
 }
