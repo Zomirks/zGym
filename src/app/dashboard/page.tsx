cfcb02f71c852aa1c13965295b81eec515cf689a
Cyril Fischer <github@cyril-fischer.fr>
2026-02-22 16:47:00 +0100

    feat: redirect not connected user

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,18 @@
-const Dashboard = () => {
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+const Dashboard = async () => {
+	const session = await auth.api.getSession({
+		headers: await headers(),
+	});
+
+	if (!session) {
+		redirect("/login");
+	}
+	
 	return (
-		<div>Dashboard</div>
+		<div>Bonjour {session.user.name!}</div>
 	)
 }
-export default Dashboard
+export default Dashboard;
