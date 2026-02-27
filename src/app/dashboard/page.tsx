1fd68e8a6d012319fb8b8b68e1870bc15ffa9481
Cyril Fischer <github@cyril-fischer.fr>
2026-02-27 14:06:58 +0100

    feat(dashboard): add Tabs

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,6 +7,14 @@ import WeightCalendar from "@/components/shared/weight/WeightCalendar";
 import WeightForm from "@/components/shared/weight/WeightForm";
 import WeightLastEntries from "@/components/shared/weight/WeightLastEntries";
 import WeightChart from "@/components/shared/weight/WeightChart";
+import {
+	Tabs,
+	TabsContent,
+	TabsList,
+	TabsTrigger,
+} from "@/components/ui/tabs"
+import { Card } from "@/components/ui/card";
+
 // LIB
 import { getWeightEntries } from "@/lib/services/weight.service";
 
@@ -21,7 +29,7 @@ const Dashboard = async () => {
 	if (!session) {
 		redirect("/login");
 	}
-	
+
 	const entries = await getWeightEntries(session.user.id, 7);
 	const chartData: WeightDataPoint[] = entries
 		.map((entry) => ({
@@ -38,29 +46,38 @@ const Dashboard = async () => {
 			Bonjour {session.user.name!}
 			
 			<div className="grid grid-cols-5 gap-5">
-				<div>
-					<h2 className="text-2xl font-semibold mb-4">Poids</h2>
-					<div className="flex flex-col gap-4 text-sm">
-						<p>Si on a un objectif de prise de masse ou de perte de poids, il faut se peser régulièrement.</p>
-						<p>Une seule pesée ne veut rien dire : notre poids varie chaque jour (eau, sel, digestion, repas plus riche…). Se peser une fois par mois peut nous tromper — surtout si ça tombe juste après un écart — alors que notre progression était bonne.</p>
-						<p>En se pesant souvent, on fais une moyenne sur la semaine et on observes la vraie tendance, pas les fluctuations.</p>
-					</div>
-								<WeightChart data={chartData} />
-				</div>
-
 				<div></div>
 
-				<div>
-				</div>
+				<div></div>
+				
+				<Tabs defaultValue="weight" className="col-span-3">
+					<TabsList className="self-end">
+						<TabsTrigger value="weight">Poids</TabsTrigger>
+						<TabsTrigger value="calories">Calories</TabsTrigger>
+					</TabsList>
+					<TabsContent value="weight" className="grid grid-cols-6 gap-5 bg-zgym-focus rounded-xl p-4">
+						<div className="flex flex-col gap-4 col-span-4">
+								<Card className="p-6">
+									<h2 className="text-4xl text-primary font-poster font-semibold">Poids</h2>
+									<div className="flex flex-col gap-4 text-sm">
+										<p>Si on a un objectif de prise de masse ou de perte de poids, il faut se peser régulièrement.</p>
+										<p>Une seule pesée ne veut rien dire : notre poids varie chaque jour (eau, sel, digestion, repas plus riche…). Se peser une fois par mois peut nous tromper — surtout si ça tombe juste après un écart — alors que notre progression était bonne.</p>
+										<p>En se pesant souvent, on fais une moyenne sur la semaine et on observes la vraie tendance, pas les fluctuations.</p>
+									</div>
+								</Card>
+								<WeightChart data={chartData} />
+							</div>
 
-				<div>
-					<WeightForm />
-				</div>
+							<div className="flex flex-col gap-4 col-span-2">
+								<WeightForm />
+								<WeightLastEntries />
+								<WeightCalendar />
+							</div>
+					</TabsContent>
 
-				<div className="flex flex-col gap-4">
-					<WeightCalendar />
-					<WeightLastEntries />
-				</div>
+					<TabsContent value="calories">Calories Tab</TabsContent>
+				</Tabs>
+
 			</div>
 		</div>
 	)
